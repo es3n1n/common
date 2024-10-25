72411561c1584f984adb328b6e4afa47ac5026ed
Arsenii es3n1n <me@es3n.in>
2024-10-25 14:53:00 +0200

    feat(macros): add packed macros

--- include/es3n1n/common/macros.hpp
+++ include/es3n1n/common/macros.hpp
@@ -2,3 +2,14 @@
 
 #define COMMON_CAT_(x, y) x##y
 #define COMMON_CAT(x, y) COMMON_CAT_(x, y)
+
+/// \note @es3n1n: I don't feel like including the whole platform.hpp for a single check.
+#if defined(_MSC_VER)
+    #define COMMON_PACKED(d) __pragma(pack(push, 1)) d __pragma(pack(pop))
+    #define COMMON_PACKED_START __pragma(pack(push, 1))
+    #define COMMON_PACKED_END __pragma(pack(pop))
+#else
+    #define COMMON_PACKED(d) d __attribute__((packed))
+    #define COMMON_PACKED_START _Pragma("pack(push, 1)")
+    #define COMMON_PACKED_END _Pragma("pack(pop)")
+#endif
---
+++ tests/macros.cpp
@@ -0,0 +1,39 @@
+#include <cstdint>
+#include <es3n1n/common/macros.hpp>
+
+struct Aligned1 {
+    std::uint8_t a;
+    std::uint16_t b;
+    std::uint16_t c;
+};
+
+COMMON_PACKED(struct Unaligned1 {
+    std::uint8_t a;
+    std::uint16_t b;
+    std::uint16_t c;
+});
+
+struct Aligned2 {
+    std::uint8_t a;
+    std::uint16_t b;
+    std::uint16_t c;
+};
+
+COMMON_PACKED_START;
+struct Unaligned2 {
+    std::uint8_t a;
+    std::uint16_t b;
+    std::uint16_t c;
+} COMMON_PACKED_END;
+
+struct Aligned3 {
+    std::uint8_t a;
+    std::uint16_t b;
+    std::uint16_t c;
+};
+
+static_assert(sizeof(Aligned1) == 6);
+static_assert(sizeof(Aligned2) == 6);
+static_assert(sizeof(Aligned3) == 6);
+static_assert(sizeof(Unaligned1) == 5);
+static_assert(sizeof(Unaligned2) == 5);
